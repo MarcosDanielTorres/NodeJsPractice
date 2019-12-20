8e16d02c46fa0780fe8ded3d13d39a38c86e4962
Marcos Daniel Torres <mdaniel.torres7@gmail.com>
2019-12-20 04:12:47 -0300

    re vergga esto

--- app.js
+++ app.js
@@ -29,3 +29,4 @@ console.log("This is a third test");
 // this is another third comment in dev branch
 // this is another forth comment in dev branch
 // lets see what happens when i commmit this in master. probably gonna be added all those lines above and this one as well
+// again...
